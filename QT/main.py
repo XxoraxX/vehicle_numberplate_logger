f4057b9644dd82f49eb89bd1d48e0c15d5a8fa22
chrissunny94 <chrissunny94@gmail.com>
2018-05-31 23:05:09 +0530

    Added HOG to the main code

--- QT/main.py
+++ QT/main.py
@@ -5,6 +5,7 @@ import imutils
 import cv2
 from read_number_plate import *
 from pyANPD import *
+import dlib
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -13,6 +14,11 @@ ap.add_argument("-v", "--video",
 ap.add_argument("-b", "--buffer", type=int, default=64,
     help="max buffer size")
 args = vars(ap.parse_args())
+
+
+#load car detector
+detector = dlib.fhog_object_detector("../DATA/SVM/car_detector.svm")
+win = dlib.image_window()
 
 if not args.get("video", False):
     video_camera = VideoCamera(0)
@@ -27,28 +33,42 @@ car_detector = carDetector()
 def loop():
 	frame = video_camera.get_frame()
 	cv2.imshow("input",frame)
-	 
-	car_detector.update_frame(frame)
 
 	try:
-		car_detector.detect_car()
-		#cv2.imshow("output",car_detector.get_detected_car())
+		dets = detector(frame)
+		for d in dets:
+			cv2.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (0, 0, 255), 2)
+			print (int(d.left()), int(d.top()) ), (int(d.right()), int(d.bottom()) )
+		frame = frame[int(d.top()): int(d.left()), int(d.bottom()):int(d.right())]
+		cv2.imshow("HOG output",frame)
+		try: 
+			output = process_image(frame, 0, type='rect')
+			cv2.imshow("Detected Number plate",output)
+			try:
+				threshold_img = preprocess(output)
+				contours= extract_contours(threshold_img)
+				cleanAndRead(car_detector.get_detected_car(),contours)
+
+			except:
+				print "Number plate reader failed"
+		except:
+			print "Plate detector failed"
+			
+			
 	except:
-		print "car_detector failed"
+		print "HOG detector failed"
+
+     
+	#car_detector.update_frame(frame)
+
+	#try:
+	#	car_detector.detect_car()
+		#cv2.imshow("output",car_detector.get_detected_car())
+	#except:
+	#	print "car_detector failed"
 
 	
-	try: 
-		output = process_image(frame, 0, type='rect')
-		cv2.imshow("Detected Number plate",output)
-		try:
-			threshold_img = preprocess(output)
-			contours= extract_contours(threshold_img)
-			cleanAndRead(car_detector.get_detected_car(),contours)
-
-		except:
-			print "Number plate reader failed"
-	except:
-		print "Plate detector failed"
+	
 			
 	
 
